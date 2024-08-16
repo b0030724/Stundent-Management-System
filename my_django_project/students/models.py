2286f29804cf331133ff4627bf0d2ac2ee0dad61
b0030724 <b0030724@my.shu.ac.uk>
2024-08-17 00:50:41 +0100

    updated models

--- my_django_project/students/models.py
+++ my_django_project/students/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils import timezone  # Import timezone for default date setting
 
 class Student(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)    
