8dfff691dc7905bc9d4ea7f2b40c50cf810a0c89
b0030724 <b0030724@my.shu.ac.uk>
2024-09-04 22:05:03 +0100

    loading to Azure storages

--- my_django_project/my_django_project/settings.py
+++ my_django_project/my_django_project/settings.py
@@ -97,9 +97,14 @@ WSGI_APPLICATION = 'my_django_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sms',
+        'PORT': '3306',
+        'HOST': 'b0030724.mysql.database.azure.com',
+        'USER': 'b0030724',
+        'PASSWORD': 'H4ll4mL4bsUser!',
+        
     }
 }
 
